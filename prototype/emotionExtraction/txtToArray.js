d91a600ae3d00f6b1481866d42aeb74dfbecab52
Amanda Clement <amanda.clement22@hotmail.com>
2022-11-30 00:12:08 -0500

    Progress on language processing: writing to JSON file and getting Tweet locations

--- prototype/emotionExtraction/txtToArray.js
+++ prototype/emotionExtraction/txtToArray.js
@@ -38,7 +38,8 @@ const files = [
   'emotionTxtFiles/fear.txt',          // [1]
   'emotionTxtFiles/joy.txt',           // [2]
   'emotionTxtFiles/sadness.txt',       // [3]
-  'emotionTxtFiles/trust.txt'          // [4]
+  'emotionTxtFiles/trust.txt',         // [4]
+  'countries.txt'                      // [5]
 ];
 
 const numTweetsToProcess = 3500;
@@ -168,56 +169,77 @@ let trustPromise = new Promise((resolve, reject) => {
   const contents = fsPromises.readFile(files[4], 'utf-8');
   resolve(contents);
 });
+let countriesPromise = new Promise((resolve, reject) => {
+  const contents = fsPromises.readFile(files[5], 'utf-8');
+  resolve(contents);
+});
 
 // Promise.all() method takes an iterable of promises as input and returns a single Promise
-Promise.all([angerPromise, fearPromise, joyPromise, sadnessPromise, trustPromise]).then((emotions) => {
+Promise.all([angerPromise, fearPromise, joyPromise, sadnessPromise, trustPromise, countriesPromise]).then((fileContents) => {
   console.log("Read emotion files");
 
   // Promise responsible for fetching Tweets
   let tweetPromise = new Promise((resolve, reject) => {
     // Get the text field value from each Tweet, store the response in a TweetObjects variable 
-    TweetsModule.find({}, { text: 1, _id: 0 }).then((response)=>{ 
+    // TweetsModule.find({}, { text: 1, _id: 0 }).then((response)=>{ 
+    TweetsModule.find({}, { date: 4, user_followers: 3, user_location: 2, text: 1, _id: 0 }).then((response)=>{ 
       const TweetObjects = response;
 
-      // Map these objects into an array (TweetList)
-      const TweetList = TweetObjects.map(
-        (tweet) => tweet.text
-      );
+      // Map these objects into arrays
+      const TweetText = TweetObjects.map( (tweet) => tweet.text );
+      const TweetLocation = TweetObjects.map( (tweet) => tweet.user_location );
+      const TweetFollowers = TweetObjects.map( (tweet) => tweet.user_followers );
+      const TweetDate = TweetObjects.map( (tweet) => tweet.date );
 
-      // TweetList array is what gets returned by the Promise (if resolved)
-      resolve(TweetList);
+      const allTweetInfo = [ TweetText, TweetLocation, TweetFollowers, TweetDate ];
+
+      // allTweetInfo array is what gets returned by the Promise (if resolved)
+      resolve(allTweetInfo);
     })
   });
   
   tweetPromise.then((tweets) => {
     console.log("Read Tweets");
+    let texts = tweets[0];
+    let locations = tweets[1];
+    let followers = tweets[2];
+    let dates = tweets[3];
 
     // An array to hold all arrays (to simplify iteration process)
     let emotionsArray = [];
 
     // Populate emotionsArray with subarrays, each representing one type of emotion, based on each txt file
-    for (let i = 0; i < emotions.length; i++)
-      emotionsArray[i] = (emotions[i]).split(/\r?[ \t][0-9][\n]/); // get full words instead of individual letters
+    for (let i = 0; i < fileContents.length - 1; i++)
+      emotionsArray[i] = (fileContents[i]).split(/\r?[ \t][0-9][\n]/); // get full words instead of individual letters
 
-    // Stemming the emotion words // creates too many issues/inaccuracies
-    // for (let i = 0; i < emotionsArray.length; i++)
-    //   for (let j = 0; j < emotionsArray[i].length; j++)
-    //     emotionsArray[i][j] = natural.PorterStemmer.stem(emotionsArray[i][j]);
+    // Last element in fileContents array is the list of countries
+    // First split it based on line breaks so that we get elements like: Afghanistan=AF
+    let countries = (fileContents[fileContents.length - 1]).split(/\r?\n/);
+
+    // Then, split each element based on =
+    // Then, if word on LHS (country name) is found in the Tweet's user location, what actually gets stored in the array is the element on RHS (country code)
+    for (let i = 0; i < countries.length; i++)
+      countries[i] = (countries[i]).split(/\r?=/);
 
     // Array of arrays, where each subarray will hold: [tweet, angerWord, anticipationWord, disgustWord, fearWord, joyWord, sadnessWord, surpriseWord, trustWord]
     // If all emotion words are blank, it means the tweet is considered neutral
     let tweetData = [];
 
+
+    // A variable that contains an array of Tweets (to be populated) for JSON file
+    let obj = {
+      tweets: []
+    };
+
     // Iterate over text from Tweets
-    for (let i = 0; i < 100; i++)
-    {
+    for (let i = 0; i < 10; i++) {
         // If an emotion word (from one of the txt files) is found in the Tweet, that word gets assigned to its appropriate string(s)
         // Only the first word related to a given emotion is retained (e.g. if there are two anger words in a Tweet, we only retain the one that comes first),
         // and is stored in its appropriate position within the subarray: [anger, fear, joy, sadness, trust]
-        let emotionWords = ['','','','',''];
+        let emotionWords = [0, 0, 0, 0, 0];
 
         // Process the Tweet text
-        const lexData = convertToStandard(tweets[i]);
+        const lexData = convertToStandard(texts[i]);
         const lowerCaseData = convertTolowerCase(lexData);
         const onlyAlpha = removeNonAlpha(lowerCaseData);
         const filteredData = stopwords.cleanText(onlyAlpha);
@@ -235,8 +257,7 @@ Promise.all([angerPromise, fearPromise, joyPromise, sadnessPromise, trustPromise
         // Handle negations: if a negation word is found, remove the tokenized word that comes directly after it
         for (let j = 0; j < tokenizedData.length; j++)
           for (let k = 0; k < negationWords.length; k++)
-            if (tokenizedData[j] == negationWords[k])
-            {
+            if (tokenizedData[j] == negationWords[k]) {
               tokenizedData.splice(j, 2);
               j = j - 2; // Decrement by two as the tokenizedData array elements were shifted by two positions,
                          // so we need to recheck the new values in these positions
@@ -244,43 +265,53 @@ Promise.all([angerPromise, fearPromise, joyPromise, sadnessPromise, trustPromise
 
         // Iterate over each Tweet's tokenized text to search for emotions (so for matches between Tweet tokens and words in txt files)
         // If a match is found, retain that word to be pushed into the tweetData array in its appropriate position within the subarray
-        for (let j = 0; j < tokenizedData.length; j++) {
+        for (let j = 0; j < tokenizedData.length; j++) 
           for (let n = 0; n < emotionsArray.length; n++)
             for (let k = 0; k < (emotionsArray[n]).length; k++)
                 if (tokenizedData[j] == (emotionsArray[n])[k]) {
-                  emotionWords[n] = (emotionsArray[n])[k];
+                  // emotionWords[n] = (emotionsArray[n])[k];
+                  emotionWords[n] = 1;
                   break;
                 }
-          
-          // Push resulting array into tweetData array
-          tweetData[i] = ([tweets[i], emotionWords[0], emotionWords[1], emotionWords[2], emotionWords[3], emotionWords[4]]);
-        }
-    }
+        
+        // Location string is filled if a country name is recognized within the tweet location text
+        let location = '';
 
-    let neutralCount = 0;
+        // Only execute the following loop if the location field isn't empty (so undefined)
+        if (typeof locations[i] != 'undefined') {
+          // Convert tweet location to lowercase
+          const lowercaseLocation = convertTolowerCase(locations[i]);
 
-    // Display tweetData array contents
-    for (let i = 0; i < 100; i++) {
-      if (tweetData[i][0] != '') 
-        console.log("TWEET: " + tweetData[i][0]);
-      // If no emotion words were found in the Tweet text, just display NEUTRAL
-      if (tweetData[i][1] == '' && tweetData[i][2] == '' && tweetData[i][3] == '' && tweetData[i][4] == '' && tweetData[i][5] == '') {
-        console.log("NEUTRAL");
-        neutralCount++;
-      }
-      if (tweetData[i][1] != '') 
-        console.log("ANGER: " + tweetData[i][1]);
-      if (tweetData[i][2] != '') 
-        console.log("FEAR: " + tweetData[i][2]);
-      if (tweetData[i][3] != '') 
-        console.log("JOY: " + tweetData[i][3]);
-      if (tweetData[i][4] != '') 
-        console.log("SADNESS: " + tweetData[i][4]);
-      if (tweetData[i][5] != '') 
-        console.log("TRUST: " + tweetData[i][5]);
-      
-      console.log("\n\n\n");
-    }
-    console.log("Neutral count is " + neutralCount);
+          // Check if tweet location text includes a full country name from countries.txt file
+          // If country name is found, assign the country code to the location variable
+          for (let k = 0; k < countries.length; k++)
+            if ((countries[k][0]).length > 1 && lowercaseLocation.includes(countries[k][0])) {
+              console.log(countries[k][0] + " found in " + lowercaseLocation);
+              location = countries[k][1]; 
+              break;
+            }
+        } // closing locations loop
+              
+        // Push resulting array into tweetData array
+        tweetData[i] = ([texts[i], emotionWords[0], emotionWords[1], emotionWords[2], emotionWords[3], emotionWords[4], location, followers[i]]);
+        obj.tweets.push({tweet: texts[i], anger: emotionWords[0], fear: emotionWords[1], joy: emotionWords[2], sadness: emotionWords[3], trust: emotionWords[4], location: location, followers: followers[i]});
+    } // closing tweet processing loop
+
+
+    let json = JSON.stringify(obj);
+    let fs = require('fs');
+    fs.writeFileSync('processedTweets.json', json);
+    
+    // TO APPEND TO FILE:
+    // fs.readFile('myjsonfile.json', 'utf8', function readFileCallback(err, data){
+    //   if (err){
+    //       console.log(err);
+    //   } else {
+    //   obj = JSON.parse(data); //now it an object
+    //   obj.tweets.push({id: 2, square:3}); //add some data
+    //   json = JSON.stringify(obj); //convert it back to json
+    //   fs.writeFileSync('myjsonfile.json', json); // write it back 
+    // }});
   });
+
 })
