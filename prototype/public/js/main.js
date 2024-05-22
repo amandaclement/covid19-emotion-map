4c70205e7470769c32ac1f80d45d524f74642c6f
Amanda Clement <amanda.clement22@hotmail.com>
2024-05-22 12:48:33 -0400

    Refactor setup() and draw() into modular functions

--- prototype/public/js/main.js
+++ prototype/public/js/main.js
@@ -1,33 +1,46 @@
 // This file contains functions for writing/drawing to the HTML page
 
+
+/**************************************************************
+ * IMPORTS
+ **************************************************************/
+import { COLOR_MAP } from './data.js';
 import Circle from './classes/Circle.js';
 import Cluster from './classes/Cluster.js';
 import FilterButton from './classes/Button.js';
 
+
+/**************************************************************
+ * CONSTANTS & VARIABLES
+ **************************************************************/
+
+// HTML elements
 const intro = document.getElementById('intro');
 const form = document.getElementById('form');
 const introScreen = document.getElementById("introScreen");
 const feedback = document.getElementById("inputNum");
 const submit = document.getElementById("submit");
+const legendElement = document.getElementById("legend");
 
-const BUTTON_POS_X = 75, BUTTON_POS_Y = 350, BUTTON_Y_OFFSET = 50;
-
-// Fade out intro text 6 seconds after page load, then update title text and fade in form
-window.onload = function() {
-    window.setTimeout(function() {
-        intro.style.opacity = '0'; 
-    }, 6000);
-    window.setTimeout(function() {
-        intro.textContent = "Please enter the number of Tweets you wish to be featured in the visualization (between 500 and 50,000):";
-        intro.style.opacity = '1'; 
-        form.style.opacity = '1';  
-    }, 7000);
-}
-
-// A boolean that's set to true once the promise returns, to ensure p5 setup and canvas drawing receive necessary info before executing
+// Updated once promise returns to ensure p5 setup and canvas drawing receive necessary info before executing
 let started = false;
 
-// Variables to store Tweet results in
+// Intro
+const MIN_TWEETS = 500, MAX_TWEETS = 500000;
+
+// Buttons
+const BUTTON_POS_X = 75, BUTTON_POS_Y = 350, BUTTON_Y_OFFSET = 50;
+
+// Emotion categories
+let emotionGroups = {
+    anger: {circles: [], clusters: [], display: true},
+    fear: {circles: [], clusters: [], display: true},
+    joy: {circles: [], clusters: [], display: true},
+    sadness: {circles: [], clusters: [], display: true},
+    neutral: {circles: [], clusters: [], display: true} 
+};
+
+// Tweets
 let totalTweets = 0;
 let tweets = {
     anger: [],
@@ -36,21 +49,195 @@ let tweets = {
     sadness: [],
     neutral: []
 };
-let locations = {};
-const locationPercentageMin = 0.01;
 
-let promise = new Promise(function(resolve, reject) {
+// Clusters
+let locations = {};
+const LOCATION_PERCENTAGE_MIN = 0.01;
+
+// Canvas drawing
+let textDiv;
+let displayClusters = false, displayCircles = true;
+
+
+/**************************************************************
+ * FUNCTIONS
+ **************************************************************/
+
+// Fade out elements
+function fadeOut(elements, delay, hide) {
+    elements.forEach(element => {
+        setTimeout(() => { 
+            element.style.opacity = '0'; 
+        }, delay);
+    }); 
+}
+
+// Fade in elements
+function fadeIn(elements, delay) {
+    elements.forEach(element => {
+        setTimeout(() => { 
+            element.style.opacity = '1'; 
+        }, delay);
+    }); 
+}
+
+// Update an element's text content
+function updateText(element, delay, newText) {
+    setTimeout(() => { 
+        element.textContent = newText;
+    }, delay);
+}
+
+// Set up p5 canvas
+function setupCanvas(x, y, parentName, backgroundColor) {
+    const myCanvas = createCanvas(x, y);
+    myCanvas.parent(parentName);
+    background(backgroundColor);
+    noStroke(); 
+}
+
+// Create child div
+function createChildDiv(parentName, text) {
+    return createDiv()
+        .parent(parentName)
+        .html(text);
+}
+
+// Get emotion color from color map
+function getColor(emotion) {
+    const [red, green, blue] = COLOR_MAP[emotion];
+    return color(red, green, blue); 
+}
+
+// Create legend in format category:total (percentage)
+function createLegend(groups) {
+    for (const group in groups) {
+        // Get # Tweet associated with this emotion
+        const emotionTotal = groups[group].length;
+
+        // Calculate emotion percentage, rounded to nearest whole #
+        const emotionPercentage = Math.round((emotionTotal / totalTweets) * 100);
+
+        // Create legend entry
+        const entry = createDiv()
+                      .html(group + ': ' + emotionTotal + ' (' + emotionPercentage + '%)')
+                      .style('color', getColor(group));
+
+        // Append to HTML legend element
+        entry.parent(legendElement);
+    }
+}
+
+// Display all emotion content
+function resetEmotionFilters() {
+    for (const emotion in emotionGroups) {
+        emotionGroups[emotion].display = true;
+    }
+}
+
+// Create canvas buttons
+function createButtons(labels) {
+    // Filter buttons
+    let buttonIndex = 0;
+    for (const label in labels) {
+        new FilterButton(label, BUTTON_POS_X, BUTTON_POS_Y + BUTTON_Y_OFFSET * buttonIndex++, () => {
+            labels[label].display = !labels[label].display
+        });
+    }
+
+    // Cluster button
+    new FilterButton('country cluster', BUTTON_POS_X, BUTTON_POS_Y + BUTTON_Y_OFFSET * buttonIndex++, () => {
+        displayCircles = !displayCircles;
+        displayClusters = !displayClusters;
+    });
+
+    // Reset button
+    new FilterButton('reset', BUTTON_POS_X, BUTTON_POS_Y + BUTTON_Y_OFFSET * buttonIndex++, () => {
+        displayCircles = true;
+        displayClusters = false;
+        resetEmotionFilters();
+    });
+}
+
+// Create a Circle for each Tweet, adding it to circles array
+function createCircles() {
+    for (const emotion in tweets) {
+        tweets[emotion].forEach(tweet => {
+            emotionGroups[emotion].circles.push(new Circle(tweet.text, textDiv, tweet.retweets, emotion));
+        });
+    }
+}
+
+// Create a Cluster for each location, adding it to clusters array
+function createClusters() {
+    for (const location in locations) {
+        const emotionCounts = locations[location];
+        let predominantEmotion, predominantEmotionCount = 0;
+        let total = 0;
+
+        // Find the predominant emotion and total Tweet count associated with this location
+        for (const emotion in emotionCounts) {
+            const count = emotionCounts[emotion];
+            total += count;
+
+            if (predominantEmotionCount < count) {
+                predominantEmotionCount = count;
+                predominantEmotion = emotion;
+            }
+        }
+
+        // Add current location to clusters array only if its total count meets the minimum threshold
+        if (total/totalTweets > LOCATION_PERCENTAGE_MIN) {
+            emotionGroups[predominantEmotion].clusters.push(new Cluster(location, total, predominantEmotion));
+        }
+    }   
+}
+
+// Draw objects of a given type
+function drawObjects(objects) {
+    for (let i = 0; i < objects.length; i++) {
+        const object = objects[i];
+        object.display();
+        object.move();
+        // Check if object has an interact method
+        if (object.interact) {
+            object.interact();
+        }
+    }
+}
+
+// Display emotion groups by drawing appropriate objects to canvas
+function displayEmotionGroups() {
+    for (const emotion in emotionGroups) {
+        const group = emotionGroups[emotion];
+        // Display emotion group only if not filtered out
+        if (group.display) {
+            if (displayCircles) {
+                drawObjects(group.circles);  // Draw Tweet circles
+            } else if (displayClusters) {
+                drawObjects(group.clusters); // Draw location clusters
+            }
+        }
+    }
+}
+
+/**************************************************************
+ * MAIN EXECUTION
+ **************************************************************/
+
+// Fade out intro text page load, then update title text and fade in form
+window.onload = () => {
+    fadeOut([intro], 6000);
+    updateText(intro, 7000, `Please enter the number of Tweets you wish to be featured in the visualization (between ${MIN_TWEETS} and ${MAX_TWEETS}):`);
+    fadeIn([intro, form], 7000);
+}
+
+ let promise = new Promise(function(resolve, reject) {
     submit.addEventListener("click",() => {
         // Fade out intro text, then fade in loading text
-        setTimeout(function() { 
-            intro.style.opacity = '0'; 
-            form.style.opacity = '0'
-        }, 100);
-
-        setTimeout(function() { 
-            intro.textContent = "Loading . . ."; 
-            intro.style.opacity = '1'; 
-        }, 1100); 
+        fadeOut([intro, form], 100);
+        updateText(intro, 1100, 'Loading . . .');
+        fadeIn([intro], 1100);
 
         // Send POST request to server
         const options = {
@@ -110,148 +297,45 @@ let promise = new Promise(function(resolve, reject) {
     });
 });
 
-// Declare variables
-let legendAnger, legendFear, legendJoy, legendSadness, legendNeutral;
-let buttonAnger, buttonFear, buttonJoy, buttonSadness, buttonNeutral, buttonReset, buttonCluster;
-let sampleInfo, textDiv;
-let displayClusters = false, displayCircles = true;
 
-let emotionGroups = {
-    anger: {circles: [], clusters: [], display: true},
-    fear: {circles: [], clusters: [], display: true},
-    joy: {circles: [], clusters: [], display: true},
-    sadness: {circles: [], clusters: [], display: true},
-    neutral: {circles: [], clusters: [], display: true} 
-};
+/**************************************************************
+ * P5 SETUP
+ **************************************************************/
 
-// Display all emotion content
-function resetEmotionFilters() {
-    for (const emotion in emotionGroups) {
-        emotionGroups[emotion].display = true;
-    }
-}
-
-// Initialize canvas buttons
-function initializeButtons() {
-    // Tweet filter buttons
-    let buttonIndex = 0;
-    for (const emotion in emotionGroups) {
-        new FilterButton(emotion, BUTTON_POS_X, BUTTON_POS_Y + BUTTON_Y_OFFSET * buttonIndex++, () => {
-            emotionGroups[emotion].display = !emotionGroups[emotion].display
-        });
-    }
-
-    // Cluster button
-    new FilterButton('country cluster', BUTTON_POS_X, BUTTON_POS_Y + BUTTON_Y_OFFSET * buttonIndex++, () => {
-        displayCircles = !displayCircles;
-        displayClusters = !displayClusters;
-    });
-
-    // Reset button
-    new FilterButton('reset', BUTTON_POS_X, BUTTON_POS_Y + BUTTON_Y_OFFSET * buttonIndex++, () => {
-        displayCircles = true;
-        displayClusters = false;
-        resetEmotionFilters();
-    });
-}
-
-// Set up the canvas
 async function setup() {
     // Await for promise to return
     await promise;
 
     // Fade out loading screen
-    setTimeout(function() { introScreen.style.opacity = '0'; }, 1000);
+    fadeOut([introScreen]);
     introScreen.style.display = 'none';
     started = true;
-    
-    // Log the # of tweets for each category along with their percentages
-    for (const emotion in tweets) {
-        console.log(tweets[emotion].length);
-    }
-    console.log("Total " + totalTweets);
 
-    // Prepare canvas
-    const myCanvas = createCanvas(700, 700);
-    myCanvas.parent('myCanvas');
-    background(255);
-    noStroke();
+    // Create p5 canvas
+    setupCanvas(700, 700, 'myCanvas', 255);
 
-    // Create a div for displaying the Tweet text, and make it a child of the tweetText div
-    textDiv = createDiv().parent('tweetText');
+    // Create divs for Tweet text and sample size
+    textDiv = createChildDiv('tweetText', '');
+    createChildDiv('sampleInfo', 'Sample size: ' + totalTweets + ' Tweets');
 
-    // Set up the emotion percentage legend
-    legendAnger = createDiv().parent('anger').html("Anger: " + tweets.anger.length);
-    legendFear = createDiv().parent('fear').html("Fear: " + tweets.fear.length);
-    legendJoy = createDiv().parent('joy').html("Joy: " + tweets.joy.length);
-    legendSadness = createDiv().parent('sadness').html("Sadness: " + tweets.sadness.length);
-    legendNeutral = createDiv().parent('neutral').html("Neutral: " + tweets.neutral.length);
-    
-    // Set up div to hold sample size
-    sampleInfo = createDiv().parent('sampleInfo').html("Sample size: " + totalTweets + " Tweets");
-
-    // Create a Circle for each Tweet, adding it to circles array
-    for (const emotion in tweets) {
-        tweets[emotion].forEach(tweet => {
-            emotionGroups[emotion].circles.push(new Circle(tweet.text, textDiv, tweet.retweets, emotion));
-        });
-    }
-
-    // Create a Cluster for each location, adding it to clusters array
-    for (const location in locations) {
-        const emotionCounts = locations[location];
-        let predominantEmotion, predominantEmotionCount = 0;
-        let total = 0;
-
-        // Find the predominant emotion and total Tweet count associated with this location
-        for (const emotion in emotionCounts) {
-            const count = emotionCounts[emotion];
-            total += count;
-
-            if (predominantEmotionCount < count) {
-                predominantEmotionCount = count;
-                predominantEmotion = emotion;
-            }
-        }
-
-        // Add current location to clusters array only if its total count meets the minimum threshold
-        if (total/totalTweets > locationPercentageMin) {
-            emotionGroups[predominantEmotion].clusters.push(new Cluster(location, total, predominantEmotion));
-        }
-    }
-
-    initializeButtons();
+    // Create emotion legend, Tweet circles, location clusters, and buttons
+    createLegend(tweets);
+    createCircles();
+    createClusters();
+    createButtons(emotionGroups);
 }
 
-// Draw to the canvas
+/**************************************************************
+ * P5 DRAW
+ **************************************************************/
+
 function draw() { 
     if (started) {
         background(255);
         translate(width/2,height/2);
 
         // Display each group (emotion) on canvas
-        for (const emotion in emotionGroups) {
-            const group = emotionGroups[emotion];
-            // Display emotion group only if not filtered out
-            if (group.display) {
-                if (displayCircles) {
-                    // Display Tweet circles
-                    for (let i = 0; i < group.circles.length; i++) {
-                        const circle = group.circles[i];
-                        circle.display();
-                        circle.move();
-                        circle.interact();
-                    }
-                } else if (displayClusters) {
-                    // Display location clusters
-                    for (let i = 0; i < group.clusters.length; i++) {
-                        const cluster = group.clusters[i];
-                        cluster.display();
-                        cluster.move();
-                    }
-                }
-            }
-        }
+        displayEmotionGroups();
     }
 }
 
